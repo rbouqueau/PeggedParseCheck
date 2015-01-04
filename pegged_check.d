2a859421f92287e90e6c10c8fd476049d0f5840f
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2015-01-04 11:56:21 +0100

    remove unused

--- pegged_check.d
+++ pegged_check.d
@@ -9,9 +9,6 @@ bool check(const ref ParseTree p)
 	if(!checkBoundaries(p, 0, p.input.length))
 		return false;
 
-	//if(!checkChildren(p))
-	//	return false;
-
 	return true;
 }
 
@@ -30,26 +27,6 @@ bool checkBoundaries(const ref ParseTree p, size_t begin, size_t end)
 		writeln("[Pegged] element not parsed:");
 		writeln(p.input[end .. p.end]);
 		return false;
-	}
-
-	return true;
-}
-
-bool checkChildren(const ref ParseTree p)
-{
-	if(p.children.length == 0)
-		return true;
-
-	if(!checkBoundaries(p, p.children[0].begin, p.children[p.children.length - 1].end))
-		return false;
-
-	foreach(ref child; p.children)
-	{
-		if(!child.successful)
-			return false;
-
-		if(!checkChildren(child))
-			return false;
 	}
 
 	return true;
